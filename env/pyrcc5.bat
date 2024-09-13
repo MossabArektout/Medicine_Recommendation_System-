46642ebf663b025da6206c10ca2cf877adf4efb9
mossab arektout <mossabarektout2000@gmail.com>
2024-09-13 17:12:46 +0100

    the model

---
+++ env/pyrcc5.bat
@@ -0,0 +1,1 @@
+@C:/Users/Mossab Arektout/Desktop/Medicine_Recommendation_System/env\python.exe -m PyQt5.pyrcc_main %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ env/pyuic5.bat
@@ -0,0 +1,1 @@
+@C:/Users/Mossab Arektout/Desktop/Medicine_Recommendation_System/env\python.exe -m PyQt5.uic.pyuic %1 %2 %3 %4 %5 %6 %7 %8 %9
